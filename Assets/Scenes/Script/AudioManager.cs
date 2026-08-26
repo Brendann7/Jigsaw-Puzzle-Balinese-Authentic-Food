017f31168f40450e913c28934d91aa2e2363e858
Brendan <brendandhananjaya@gmail.com>
2026-08-27 00:42:44 +0800

    feat: adding audiomanager script

---
+++ Assets/Scenes/Script/AudioManager.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+
+    public AudioSource bgmSource;
+    public AudioSource sfxSource;
+
+    public AudioClip buttonSound;
+    public AudioClip dragPuzzleSound;
+    public AudioClip dropPuzzleSound;
+    public AudioClip finishGameSound;
+    public AudioClip popUpSound;
+    public AudioClip pronounceFoodSound;
+
+    private bool isMuted = false;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void ToggleMute()
+    {
+        isMuted = !isMuted;
+        AudioListener.volume = isMuted ? 0f : 1f;
+    }
+
+    public void PlayButton()
+    {
+        if (buttonSound != null) sfxSource.PlayOneShot(buttonSound);
+    }
+
+    public void PlayDragPuzzle()
+    {
+        if (dragPuzzleSound != null) sfxSource.PlayOneShot(dragPuzzleSound);
+    }
+
+    public void PlayDropPuzzle()
+    {
+        if (dropPuzzleSound != null) sfxSource.PlayOneShot(dropPuzzleSound);
+    }
+
+    public void PlayFinishGame()
+    {
+        if (finishGameSound != null) sfxSource.PlayOneShot(finishGameSound);
+    }
+
+    public void PlayPopUp()
+    {
+        if (popUpSound != null) sfxSource.PlayOneShot(popUpSound);
+    }
+
+    public void PlayPronounceFood()
+    {
+        if (pronounceFoodSound != null) sfxSource.PlayOneShot(pronounceFoodSound);
+    }
+}
